0405e8350a397cb49f89f2dd2908399134f55827
Hangyu Xu <hangxu@microsoft.com>
2024-07-16 17:02:09 +0800

    resort required providers

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = "~> 1.7"
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 1.13"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
@@ -8,14 +12,6 @@ terraform {
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
-    }
-    azapi = {
-      source  = "azure/azapi"
-      version = "~> 1.13"
-    }
-    modtm = {
-      source  = "Azure/modtm"
-      version = "~> 0.3"
     }
   }
 }
