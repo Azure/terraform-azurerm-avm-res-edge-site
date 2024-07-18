2e6a558cb38f281053cf00165e7574dd9759a494
xhy8759 <529370469@qq.com>
2024-07-18 13:03:35 +0800

    Use new modtm (#4)
    * use new modtm
    
    ---------
    
    Co-authored-by: Hangyu Xu <hangxu@microsoft.com>

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 # TODO: insert locals here.
 locals {
-  resource_group_name                = element(split("/", var.resource_group_id), length(split("/", var.resource_group_id)) - 1)
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
 }
