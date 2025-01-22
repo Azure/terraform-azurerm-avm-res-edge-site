a118e1c65a059b1566779f547ea0a37499b2cacc
Hangyu Xu <hangxu@microsoft.com>
2025-01-22 13:05:53 +0800

    support azurerm 4.0

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -3,12 +3,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.74"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "azurerm" {
+  subscription_id = "0000000-0000-00000-000000"
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
